7ddce9f39d5dc3f4deceb6b7436f1658804a6d65
Rajesh Yadav <44490757+Rajesh3601@users.noreply.github.com>
2020-07-23 11:38:05 +0530

    Update Day 6.py

--- Day 1 - 10/Day 6.py
+++ Day 1 - 10/Day 6.py
@@ -1,2 +1,41 @@
 #Question 16
-#toDo
+##Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+#Suppose the following input is supplied to the program:
+#1,2,3,4,5,6,7,8,9
+#Then, the output should be:
+#1,3,5,7,9
+
+
+value = input()
+lst = [x for x in value.split(",") if int(x)%2!=0 ]
+print(lst)
+
+
+#Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+#D 100
+#W 200
+
+#D means deposit while W means withdrawal.
+#Suppose the following input is supplied to the program:
+#D 300
+#D 300
+#W 200
+#D 100
+#Then, the output should be:
+#500
+
+netAmount = 0
+while True:
+    s = input()
+    if not s:
+        break
+    values = s.split(" ")
+    operation = values[0]
+    amount = int(values[1])
+    if operation=="D":
+        netAmount+=amount
+    elif operation=="W":
+        netAmount-=amount
+    else:
+        pass
+print(netAmount)
